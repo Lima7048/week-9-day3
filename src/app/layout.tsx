d86786fc12212552e4480d90f749d363cdcef4f5
salima <brycesalima@gmail.com>
2024-03-07 13:08:41 +0000

    saving work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "@/app/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
+        <Nav />
       </html>
     </ClerkProvider>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import fly from "@/../public/fly.jpeg";
+import fly from "@/../public/fly.jpg";
 
 export default function Home() {
   return (
